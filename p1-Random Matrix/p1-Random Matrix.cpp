95fe2e7d88122b08b3febfbeff434569a19a41f4
Abdallahhu <abdallah.a99h@gmail.com>
2023-04-01 12:57:53 +0300

    Work

--- p1-Random Matrix/p1-Random Matrix.cpp
+++ p1-Random Matrix/p1-Random Matrix.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+int RandomNumber(int From, int To)
+{
+    int Number =rand() % (To - From + 1) + From;
+    return Number;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    cout << "The folwing is a 3*3 random matrix:\n";
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            cout << setw(5) << arr[i][j];
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    srand((unsigned)time(NULL));
+    int arr[3][3];
+    int Rows = 3, Cols = 3;
+    FillMatrixWithRandomNumbers(arr, Rows, Cols);
+    PrintMatrix(arr, Rows, Cols);
+    
+    return 0;
 }
